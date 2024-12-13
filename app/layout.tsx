00d626b423f17740bd2e0d224df5696644c56bc7
geoffrey <1377499035@qq.com>
2024-12-13 09:23:26 +0800

    complete nextjs-learn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | acme dashboard",
+    default: "acme dashboard",
+  },
+  description: "the official nextjs course dashboard, built with app router",
+  metadataBase: new URL("https://nextjs.org"),
+};
 
 export default function RootLayout({
   children,
